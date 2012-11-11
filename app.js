18e2672924666a603b6a523932f13067c070e77f
Jason Divock <jdivock@gmail.com>
2012-11-11 18:34:36 -0500

    tumblr posting working . . . sort of

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-	 OAuth = require('oauth').OAuth;
+	 OAuth = require('oauth').OAuth,
+	 Tumblr = require('tumblrwks'),
+	 querystring = require('querystring');
 
 // Setup the Express.js server
 var app = express.createServer();
@@ -11,19 +13,19 @@ app.use(express.session({
 }));
 
 // Home Page
-app.get('/', function(req, res){
-	res.send('Working on it');
-	/*if(!req.session.oauth_access_token) {
-		res.redirect("/tumblr_login");	
-		//res.redirect("/google_login");
+app.get('/', function(request, response){
+	//response.send('Working on it');
+	if(!request.session.oauth_access_token) {
+		response.redirect("/tumblr_login");	
+		//response.redirect("/google_login");
 	}
 	else {
-		res.redirect("/getTumblrInfo");
-	}*/
+		response.redirect("/getTumblrUserInfo");
+	}
 });
 
 
-app.get('/tumblr_login', function(req,res){
+app.get('/tumblr_login', function(request,response){
 	
 	var oa = new OAuth("http://www.tumblr.com/oauth/request_token",
 						"http://www.tumblr.com/oauth/access_token",
@@ -31,7 +33,7 @@ app.get('/tumblr_login', function(req,res){
 	                  process.env.MULTIPOST_SECRET_KEY,
 	                  "1.0",
 	                  "http://google.com",
-	                  //"http://localhost:3000/tumblr_cb"+( req.param('action') && req.param('action') != "" ? "?action="+querystring.escape(req.param('action')) : "" ),
+	                  //"http://localhost:3000/tumblr_cb"+( request.param('action') && request.param('action') != "" ? "?action="+querystring.escape(request.param('action')) : "" ),
 	                  "HMAC-SHA1");
 
 	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
@@ -43,35 +45,35 @@ app.get('/tumblr_login', function(req,res){
 		}
 	  else { 
 			// store the tokens in the session
-			req.session.oa = oa;
-			req.session.oauth_token = oauth_token;
-			req.session.oauth_token_secret = oauth_token_secret;
+			request.session.oa = oa;
+			request.session.oauth_token = oauth_token;
+			request.session.oauth_token_secret = oauth_token_secret;
 		
 			// redirect the user to authorize the token
-	   		res.redirect("http://www.tumblr.com/oauth/authorize?oauth_token="+oauth_token);
+	   		response.redirect("http://www.tumblr.com/oauth/authorize?oauth_token="+oauth_token);
 	  }
 	})
 })
 
 // Callback for the authorization page
-app.get('/tumblr_cb', function(req, res) {
+app.get('/tumblr_cb', function(request, response) {
 		
 	// get the OAuth access token with the 'oauth_verifier' that we received
 	
-	var oa = new OAuth(req.session.oa._requestUrl,
-	                  req.session.oa._accessUrl,
-	                  req.session.oa._consumerKey,
-	                  req.session.oa._consumerSecret,
-	                  req.session.oa._version,
-	                  req.session.oa._authorize_callback,
-	                  req.session.oa._signatureMethod);
+	var oa = new OAuth(request.session.oa._requestUrl,
+	                  request.session.oa._accessUrl,
+	                  request.session.oa._consumerKey,
+	                  request.session.oa._consumerSecret,
+	                  request.session.oa._version,
+	                  request.session.oa._authorize_callback,
+	                  request.session.oa._signatureMethod);
 	
     console.log(oa);
 	
 	oa.getOAuthAccessToken(
-		req.session.oauth_token, 
-		req.session.oauth_token_secret, 
-		req.param('oauth_verifier'), 
+		request.session.oauth_token, 
+		request.session.oauth_token_secret, 
+		request.param('oauth_verifier'), 
 		function(error, oauth_access_token, oauth_access_token_secret, results2) {
 			
 			if(error) {
@@ -81,53 +83,97 @@ app.get('/tumblr_cb', function(req, res) {
 	 		else {
 		
 				// store the access token in the session
-				req.session.oauth_access_token = oauth_access_token;
-				req.session.oauth_access_token_secret = oauth_access_token_secret;
+				request.session.oauth_access_token = oauth_access_token;
+				request.session.oauth_access_token_secret = oauth_access_token_secret;
 				
-	    		res.redirect((req.param('action') && req.param('action') != "") ? req.param('action') : "/getTumblrInfo");
+	    		response.redirect((request.param('action') && request.param('action') != "") ? request.param('action') : "/getTumblrUserInfo");
 	 		}
 
 	});
 	
 });
 
-app.get('/makeTumblrPost', function(req,res){
-	var oa = new OAuth(req.session.oa._requestUrl,
-	                  req.session.oa._accessUrl,
-	                  req.session.oa._consumerKey,
-	                  req.session.oa._consumerSecret,
-	                  req.session.oa._version,
-	                  req.session.oa._authorize_callback,
-	                  req.session.oa._signatureMethod);
 
+app.post('/postTumblr', require_tumblr_login, function(request, response){
+		tumblr =  new Tumblr(
+						  {
+						    consumerKey: request.session.oa._consumerKey,
+						    consumerSecret: request.session.oa._consumerSecret,
+						    accessToken: request.session.oauth_access_token,
+						    accessSecret: request.session.oauth_access_token_secret
+						  }, request.session.tumblr_blog + ".tumblr.com"
+			);
 
+		tumblr.post('/post', {type: 'text', title: request.body.title, body: request.body.body}, function(json){
+		  console.log(json);
+		});
+
+		response.send({"status":"ok"})
 })
 
-app.get('/getTumblrInfo', function(req, res){
+app.get('/getTumblrUserInfo', require_tumblr_login, function(request, response){
 
-	var oa = new OAuth(req.session.oa._requestUrl,
-	                  req.session.oa._accessUrl,
-	                  req.session.oa._consumerKey,
-	                  req.session.oa._consumerSecret,
-	                  req.session.oa._version,
-	                  req.session.oa._authorize_callback,
-	                  req.session.oa._signatureMethod);
+	var oa = new OAuth(request.session.oa._requestUrl,
+	                  request.session.oa._accessUrl,
+	                  request.session.oa._consumerKey,
+	                  request.session.oa._consumerSecret,
+	                  request.session.oa._version,
+	                  request.session.oa._authorize_callback,
+	                  request.session.oa._signatureMethod);
+
+
+	oa.getProtectedResource(
+					"http://api.tumblr.com/v2/user/info	", 
+					"GET", 
+					request.session.oauth_access_token, 
+					request.session.oauth_access_token_secret,
+					function (error, data, res) {
+						console.log("DATA: " + data);
+						var feed = JSON.parse(data);
+
+						request.session.tumblr_blog = feed.response.user.blogs[0].name;
+						console.log(request.session.tumblr_blog);
+						response.write(data);
+
+						
+		
+						//response.write(JSON.stringify(data));
+						response.end();
+					}		
+				)	
+});
+
+function require_tumblr_login(req, res, next) {
+	if(!req.session.oauth_access_token) {
+		res.redirect("/tumblr_login?action="+querystring.escape(req.originalUrl));
+		return;
+	}
+	next();
+};
+
+app.get('/getTumblrInfo', require_tumblr_login, function(request, response){
+
+	var oa = new OAuth(request.session.oa._requestUrl,
+	                  request.session.oa._accessUrl,
+	                  request.session.oa._consumerKey,
+	                  request.session.oa._consumerSecret,
+	                  request.session.oa._version,
+	                  request.session.oa._authorize_callback,
+	                  request.session.oa._signatureMethod);
 
 	console.log("GETTING TUMBLR INFO");
 
 	oa.getProtectedResource(
 					"http://api.tumblr.com/v2/user/info	", 
 					"GET", 
-					req.session.oauth_access_token, 
-					req.session.oauth_access_token_secret,
-					function (error, data, response) {
+					request.session.oauth_access_token, 
+					request.session.oauth_access_token_secret,
+					function (error, data, res) {
 						console.log("DATA: " + data);
 						var feed = JSON.parse(data);
-
-						console.log(feed.response.user.blogs[0]);
-						res.write(data);
-						//res.write(JSON.stringify(data));
-						res.end();
+						response.write(data);
+						
+						response.end();
 					}		
 				)	
 });
